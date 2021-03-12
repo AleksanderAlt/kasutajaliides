a4835431c2db540b4d81a4bbb2b0f8af9bc4db2c
AleksanderAlt <58027203+AleksanderAlt@users.noreply.github.com>
2021-03-12 15:58:11 +0200

    add input box

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-require('./main.scss');
-require('minireset.css');
+
 import Vue from 'vue';
 
-
-
 let app = new Vue ({
-    el: '#app'
+    el: '#app',
+    data: {
+        message: '',
+        items: []
+    },
+    methods: {
+        addItem(){
+            if(this.message.trim() !== '') {
+                this.items.push(this.message);
+                this.message = '';
+            }
+        }
+    }
 });
