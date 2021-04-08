1ae44773b2225e6ee494019738e6c3a7db66c731
AleksanderAlt <58027203+AleksanderAlt@users.noreply.github.com>
2021-04-08 10:45:21 +0300

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="container">
         <section class="section">
-            <button class="button is-primary">Hello</button>
+            <button class="button is-primary" @click="modalActive=true">Hello</button>
         </section>
+        <modal :active="modalActive" @close="modalActive=false"></modal>
+
+
+
     </div>
 </template>
 
 <script>
-export default {
-    name: "App",
-}
+    import Modal from "./components/Modal";
+    export default {
+        name: "App",
+        components: {Modal},
+        data(){
+            return {
+                modalActive: false
+            }
+        }
+    }
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="modal is-active" v-if="active">
+        <div class="modal-background" @click="$emit('close')"></div>
+        <div class="modal-content">
+            <p class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="">
+            </p>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Modal",
+        props: ['active']
+    }
+</script>
+
+<style scoped>
+
+</style>
